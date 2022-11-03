7727f35841540e1903dfb2644ad958ae1022aa43
Timothy Killings <akaamkafamily@Timothys-iMac.local>
2022-11-02 18:10:57 -0700

    Daily Challege for Day1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let name = "Tim";
+console.log(name);
+let food = "Thai";
+console.log(food);
+const interestRate = 2.5;
+console.log(interestRate); ;
+console.log(name);
+// Take Home Challenge Single Line Comment
+
+/* Take Home Challenge
+Multi Line Comment*/
